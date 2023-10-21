21096a166108c753615dfe4887596a4d15229cbf
Suyash Thapa <sthapa@jacobs-university.de>
2023-10-21 16:13:13 +0200

    2

--- Sheet1/Sheet1_solution.py
+++ Sheet1/Sheet1_solution.py
@@ -2,6 +2,7 @@
 import cv2 as cv
 import random as random
 import numpy as np
+import matplotlib.pyplot as plt
 
 def display(window_name, img):
     image = cv.imread(img)
@@ -45,18 +46,56 @@ def compute_mean_grey_value_1(img):
     total_pixels = integral_image.shape[0] * integral_image.shape[1]
     mean_grey = sum_grey_value / total_pixels
     return mean_grey
+
+##2a histogram equlaization 
+
+def histogram(img): 
+    hist_img = cv.equalizeHist(img) 
+    cv.imwrite('bonn_equalized.png', hist_img)
+    # Display the original and equalized images (optional)
+    return hist_img
+
+def own_histogram(img):
+    
+    hist, _ = np.histogram(img, bins=256, range=(0, 256))
+    cdf = hist.cumsum()
+    cdf_normalized = cdf * hist.max() / cdf.max()
+    
+    # Apply histogram equalization
+    equalized_image = np.interp(img, np.arange(256), cdf_normalized)
+    equalized_image = np.uint8(equalized_image)
+    
+    return equalized_image 
+
+
     
 if __name__ == "__main__":
     img = "bonn.png"
     
     # Problem 1 a. display the integral image
-    integral_image = compute_integral_image(img)
-    cv.imshow("Integral Image",integral_image)
-    cv.waitKey(0)
+    #integral_image = compute_integral_image(img)
+    #cv.imshow("Integral Image",integral_image)
+    #cv.waitKey(0)
     
     # Problem 1 b.1 display the mean grey value of the image
-    mean_grey_value = compute_mean_grey_value_1(img)
-    print("Mean grey value of the image is: ", mean_grey_value)
+    #mean_grey_value = compute_mean_grey_value_1(img)
+    #print("Mean grey value of the image is: ", mean_grey_value)
+
+    image = cv.imread(img)
+    gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY) 
+    pixel_lib = histogram(gray_image) 
+    custom_lib = own_histogram(gray_image) 
+    pixel_diff = np.abs(pixel_lib - custom_lib)
+    # Find the maximum pixel error
+    max_pixel_error = np.max(pixel_diff) 
+    cv.imshow('Equalized Image (OpenCV)', pixel_lib)
+    cv.imshow('Equalized Image (Custom)', custom_lib)
+    cv.imshow('Pixelwise Difference', pixel_diff)
+    print(f"Maximum Pixel Error: {max_pixel_error}")
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    
+
     
     
     
