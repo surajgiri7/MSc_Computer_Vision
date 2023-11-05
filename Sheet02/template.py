2667102e815c3526b6213b6ce89a976ab5f5b8cf
Suyash Thapa <sthapa@jacobs-university.de>
2023-11-05 08:51:34 +0100

    odd questions

--- Sheet02/template.py
+++ Sheet02/template.py
@@ -9,23 +9,98 @@ import time
 #                                                         #  
 ###########################################################
 
+
+def display(window_name, img):
+
+    cv2.imshow(window_name, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+
+
+
+
 def get_convolution_using_fourier_transform(image, kernel):
-    # TODO: implement
-    raise NotImplementedError
+
+    # Calculate the Fourier transforms of the image and kernel
+    ft_img = np.fft.fft2(image)
+    ft_kernel = np.fft.fft2(kernel, s=image.shape)
+
+    # Perform frequency domain multiplication
+    ft_result = ft_img * ft_kernel
+
+    # Transform the result back to the spatial domain
+    result = np.fft.ifft2(ft_result).real  # .real extracts the real part
+
+    # Normalize and convert the result to uint8
+    result = (result - result.min()) / (result.max() - result.min()) * 255
+    return result.astype(np.uint8)
 
 
-def get_convolution(image, kernel):
-    # TODO: implement
-    raise NotImplementedError
+
+def get_convolution_using_fourier_transform_correction(image, kernel):
+    # Calculate the Fourier transforms of the image and kernel
+    ft_image = np.fft.fft2(image)
+    ft_kernel = np.fft.fft2(kernel, s=image.shape)
+
+    # Perform frequency domain multiplication
+    ft_result = ft_image * ft_kernel
+
+    # Transform the result back to the spatial domain
+    result = np.fft.ifft2(ft_result).real  # .real extracts the real part
+
+    # Ensure the result is within the valid data range (0-255)
+    result = np.clip(result, 0, 255).astype(np.uint8)
+
+    return result
+
+
+def convolution_spatial_vectorized(image, kernel):
+    k_height, k_width = kernel.shape
+    k_center_x, k_center_y = k_width // 2, k_height // 2
+
+    # Pad the image to handle borders
+    padded_image = np.pad(image, ((k_center_y, k_center_y), (k_center_x, k_center_x)), mode='constant')
+
+    # Perform convolution using vectorized operations
+    result = np.zeros_like(image, dtype=np.float32)
+
+    for y in range(image.shape[0]):
+        for x in range(image.shape[1]):
+            result[y, x] = np.sum(padded_image[y:y + k_height, x:x + k_width] * kernel)
+
+    # Normalize the result to be within the 0-255 range
+    result = (result - np.min(result)) / (np.max(result) - np.min(result)) * 255
+
+    return result.astype(np.uint8)
+
+def get_convolution(image, kernel): 
+
+    result = cv2.filter2D(image, -1, kernel) 
+    return result
+    
 
 
 def task1():
     image = cv2.imread("./data/einstein.jpeg", cv2.IMREAD_GRAYSCALE)
-    kernel = None  # TODO: calculate kernel
-
-    cv_result = None # TODO: cv2.filter2D
-    conv_result = get_convolution(image, kernel)
-    fft_result = get_convolution_using_fourier_transform(image, kernel)
+    kernel = cv2.getGaussianKernel(7,1)  
+    gaussian_kernel = kernel.dot(kernel.reshape(1, 7)) 
+    conv_result = get_convolution(image, gaussian_kernel)  
+    own_conv = convolution_spatial_vectorized(image,kernel)
+    fft_result = get_convolution_using_fourier_transform(image,kernel) 
+    fft_result_correction = get_convolution_using_fourier_transform_correction(image,kernel)
+    display("original image",image) 
+    display("convolution image",conv_result) 
+    display("own function",own_conv)
+    display("fft function",fft_result) 
+    #oscillations or ripples around high-contrast edges in the image in fft. 
+    display("fft_result_correction",fft_result_correction)
+    mean_abs_diff = np.mean(np.abs(np.subtract(conv_result.astype(np.int32), fft_result.astype(np.int32))))
+    print(mean_abs_diff)
+   
+     
+   
 
     # TODO: compare results
 
@@ -150,16 +225,20 @@ def task4():
 def task5():
     image = cv2.imread("./data/traffic.jpg", cv2.IMREAD_GRAYSCALE)
 
-    edges = None  # TODO: compute edges
+    edges = cv2.Canny(image, 50, 60)
 
-    dist_transfom_cv = None  # TODO: compute using opencv
+    display("edges",edges) 
+
+    dist_transfom_cv = cv2.distanceTransform(image, cv2.DIST_L2, 0)
+
+    display("distance_cv",dist_transfom_cv)
 
     dist_transfom_cv_filtered = None  # TODO: compute after filtering some high-frequency edges
 
 
 if __name__ == "__main__":
-    task1()
-    task2()
-    task3()
-    task4()
+    #task1()
+    #task2()
+    #task3()
+    #task4()
     task5()
