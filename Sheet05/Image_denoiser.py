65130d051c7ff73d24276c3d28db582d1692a65c
Suraj Giri <surajgiri4210@gmail.com>
2023-12-03 23:14:39 +0100

    Sheet 5

--- Sheet05/Image_denoiser.py
+++ Sheet05/Image_denoiser.py
@@ -50,118 +50,75 @@ def binary_img_denoiser(img, rho, pairwise_cost_same, pairwise_cost_diff):
 
 
 
-def grayscale_img_denoiser(I,rho=0.6):
+def grayscale_img_denoiser(I, rho=0.6):
     labels = np.unique(I).tolist()
 
-    Denoised_I = np.zeros_like(I)
-    Denoised_I = I.copy()
-    ### Use Alpha expansion binary image for each label
-    #I = I/I.max()
-    num_labels = len(labels)
-    D = np.abs(I.reshape(I.shape + (1,)) - np.array(labels).reshape((1, 1, -1)))
-    D = np.where(D == 0, rho, (1-rho)/2)
-    V = 255 * np.eye(3)
+    Denoised_I = np.copy(I)
 
     max_iter = 50
     better_energy = np.inf
-    # Stop when energy is not changed or reached max iteration.
-    for i in range(max_iter):
+    for _ in range(max_iter):
         improved = False
-        # Iterate through the labels.
         for alpha in labels:
-            # Create graph and Caculate the energy.
-            energy, Denoised_I = calculate_energy(alpha, D, V, Denoised_I, rho)
-            # Check if the better energy has been improved.
+            energy, Denoised_I = calculate_energy(alpha, I, Denoised_I, rho)
             if energy < better_energy:
                 better_energy = energy
                 improved = True
-        # Finish the minimization when energy is not decreased.
         if not improved:
             break
 
-    cv2.imshow('Original Img', I), \
-    cv2.imshow('Denoised Img', Denoised_I), cv2.waitKey(0), cv2.destroyAllWindows()
+    cv2.imshow('Original Img', I)
+    cv2.imshow('Denoised Img', Denoised_I)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-    return
-
-def calculate_energy(alpha, D, V, Denoised_I, rho):
-    ### 1) Define Graph
+def calculate_energy(alpha, I, Denoised_I, rho):
     g = maxflow.Graph[float]()
-
     row, col = Denoised_I.shape
-    ### 2) Add pixels as nodes
     nodeids = g.add_grid_nodes((row, col))
-    ### 3) Compute Unary cost
-    ### 4) Add terminal edges
-    #g.add_grid_tedges(nodeids, rho, (1-rho)/2)
-    label_dict = {0:0, 128:1, 255:2}
 
-    ### 5) Add Node edges
-    ### Vertical Edges
-    for c in range(col):
-        for r in range(row-1):
-            curr_state = Denoised_I[r, c]
-            if curr_state == alpha:
-                # Add unary cost from label to source and sink
-                g.add_tedge(nodeids[r, c], D[r,c,label_dict[alpha]], np.inf)
-                if Denoised_I[r+1, c] != alpha: # for alpha and else
-                    dist_state_alpha = V[label_dict[Denoised_I[r+1, c]], label_dict[alpha]]
-                    g.add_edge(nodeids[r, c], nodeids[r+1, c], 0, dist_state_alpha)
-            else:
-                g.add_tedge(nodeids[r, c], D[r,c,label_dict[alpha]], D[r,c,label_dict[curr_state]])
-                if Denoised_I[r+1, c] == alpha: # for else and alpha
-                    dist_state_alpha = V[label_dict[Denoised_I[r, c]], label_dict[alpha]]
-                    g.add_edge(nodeids[r, c], nodeids[r+1, c], dist_state_alpha, 0)
-                else: # for else and else
-                    if Denoised_I[r, c] == Denoised_I[r+1, c]:
-                        dist_state_alpha = V[label_dict[Denoised_I[r, c]], label_dict[Denoised_I[r+1, c]]]
-                        g.add_edge(nodeids[r, c], nodeids[r + 1, c], dist_state_alpha, dist_state_alpha)
-                    else:
-                        dist = V[label_dict[Denoised_I[r, c]], label_dict[Denoised_I[r+1, c]]]
-                        curr = V[label_dict[Denoised_I[r, c]], label_dict[alpha]]
-                        next = V[label_dict[Denoised_I[r+1, c]], label_dict[alpha]]
-                        # Add an extra node between two nodes that both are not alpha
-                        extra_node = g.add_nodes(1)
-                        g.add_tedge(extra_node, 0, dist)
-                        g.add_edge(nodeids[r, c], extra_node, curr, np.inf)
-                        g.add_edge(nodeids[r+1, c], extra_node, np.inf, next)
-    ### Horizontal edges
-    # (Keep in mind the stucture of neighbourhood and set the weights according to the pairwise potential)
+    label_dict = {0: 0, 128: 1, 255: 2}
+    V = 255 * np.eye(3)
+
+    D = np.abs(I.reshape(I.shape + (1,)) - np.array([alpha]).reshape((1, 1, -1)))
+    D = np.where(D == 0, rho, (1 - rho) / 2)
+
     for r in range(row):
-        for c in range(col - 1):
-            curr_state = Denoised_I[r, c]
-            if curr_state == alpha:
-                if Denoised_I[r, c+1] != alpha:
-                    dist_state_alpha = V[label_dict[alpha], label_dict[Denoised_I[r, c+1]]]
-                    g.add_edge(nodeids[r, c], nodeids[r, c+1], 0, dist_state_alpha)
-            else:
-                if Denoised_I[r, c+1] == alpha:
-                    dist_state_alpha = V[label_dict[Denoised_I[r, c]], label_dict[alpha]]
-                    g.add_edge(nodeids[r, c], nodeids[r, c+1], dist_state_alpha, 0)
-                else:
-                    if Denoised_I[r, c] == Denoised_I[r, c+1]:
-                        dist_state_alpha = V[label_dict[Denoised_I[r, c]], label_dict[Denoised_I[r, c+1]]]
-                        g.add_edge(nodeids[r, c], nodeids[r, c+1], dist_state_alpha, dist_state_alpha)
-                    else:
-                        dist = V[label_dict[Denoised_I[r, c]], label_dict[Denoised_I[r, c+1]]]
-                        curr = V[label_dict[Denoised_I[r, c]], label_dict[alpha]]
-                        next = V[label_dict[Denoised_I[r, c+1]], label_dict[alpha]]
-                        extra_node = g.add_nodes(1)
-                        g.add_tedge(extra_node, 0, dist)
-                        g.add_edge(nodeids[r, c], extra_node, curr, np.inf)
-                        g.add_edge(nodeids[r, c+1], extra_node, np.inf, next)
+        for c in range(col):
+            add_t_edge(g, nodeids, Denoised_I, label_dict, V, alpha, r, c, rho, D)
 
-    ### 6) Maxflow
     energy = g.maxflow()
-
     segments = g.get_grid_segments(nodeids)
     segments = np.logical_not(segments)
+
     for i in range(row):
         for j in range(col):
-            if segments[i,j]:
+            if segments[i, j]:
                 Denoised_I[i, j] = alpha
 
     return energy, Denoised_I
+
+def add_t_edge(g, nodeids, Denoised_I, label_dict, V, alpha, r, c, rho, D):
+    curr_state = Denoised_I[r, c]
+    if curr_state == alpha:
+        g.add_tedge(nodeids[r, c], D[r, c, label_dict[alpha]], np.inf)
+        if r < Denoised_I.shape[0] - 1 and Denoised_I[r + 1, c] != alpha:
+            dist_state_alpha = V[label_dict[Denoised_I[r + 1, c]], label_dict[alpha]]
+            g.add_edge(nodeids[r, c], nodeids[r + 1, c], 0, dist_state_alpha)
+    else:
+        g.add_tedge(nodeids[r, c], D[r, c, label_dict[alpha]], D[r, c, label_dict[curr_state]])
+        if r < Denoised_I.shape[0] - 1 and Denoised_I[r + 1, c] == alpha:
+            dist_state_alpha = V[label_dict[Denoised_I[r, c]], label_dict[alpha]]
+            g.add_edge(nodeids[r, c], nodeids[r + 1, c], dist_state_alpha, 0)
+        else:
+            if r < Denoised_I.shape[0] - 1 and Denoised_I[r, c] != Denoised_I[r + 1, c]:
+                dist = V[label_dict[Denoised_I[r, c]], label_dict[Denoised_I[r + 1, c]]]
+                curr = V[label_dict[Denoised_I[r, c]], label_dict[alpha]]
+                next = V[label_dict[Denoised_I[r + 1, c]], label_dict[alpha]]
+                extra_node = g.add_nodes(1)
+                g.add_tedge(extra_node, 0, dist)
+                g.add_edge(nodeids[r, c], extra_node, curr, np.inf)
+                g.add_edge(nodeids[r + 1, c], extra_node, np.inf, next)
 
 
 
